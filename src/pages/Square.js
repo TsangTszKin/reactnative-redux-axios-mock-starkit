ec434343c1783d53788a498a787e59c6d6ec06c8
xiaoyou <xiaoyou.zzj@dtyunxi.com>
2021-11-25 17:43:21 +0800

    update: 广场页其他

--- src/pages/Square.js
+++ src/pages/Square.js
@@ -1,47 +1,62 @@
-/*
- * @descripttion: 
- * @author: 逍游
- * @time: Do not edit
- */
-/*
- * @descripttion: 
- * @author: 逍游
- * @time: Do not edit
- */
 import React from 'react'
-import PropTypes from 'prop-types';
-import { View, Text, Button } from 'react-native'
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from 'react-native'
 import { connect } from 'react-redux';
-import { requestCardList } from '../actions/index';
+import { requestCardList } from '../actions/'
 
 @connect(
-    state => ({
-        cardList: state.square.cardList,
-    }),
-    dispatch => ({
-        requestCardList: (...params) => dispatch(requestCardList(...params))
-    })
+  state => ({
+    cardList: state.square.cardList,
+  }),
+  dispatch => ({
+    requestCardList: (...params) => dispatch(requestCardList(...params))
+  })
 )
 class Square extends React.Component {
-    static propTypes = {
-        cardList: PropTypes.object.isRequired,
-        requestCardList: PropTypes.func.isRequired,
-    }
-    requestCardList = () => {
-        this.props.requestCardList()
-    }
-    componentDidMount() {
-        console.log('requestCardList', requestCardList)
-    }
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Square!</Text>
-                <Text>{this.props.cardList.localTotal}</Text>
-                <Button title="获取数据" onPress={this.requestCardList}></Button>
-            </View>
+            <SafeAreaView style={styles.container}>
+                  <FlatList
+                    data={DATA}
+                    renderItem={({ item }) => 
+						<View style={styles.item}>
+							<Text style={styles.title}>{item.title}</Text>
+						</View>
+					}
+                    keyExtractor={item => item.id}
+                  />
+                </SafeAreaView>
         )
     }
 }
 
 export default Square
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  title: {
+    fontSize: 32,
+  },
+});
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
